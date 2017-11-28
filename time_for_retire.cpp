412494220af3035d4671e7914ba861248ff96e73
guitsemun <guitsemun@gmail.com>
2017-11-28 04:17:10 -0500

    revision de programa 1

--- time_for_retire.cpp
+++ time_for_retire.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main()
 {
 	int age,wretire;
+	cout<<"\n------------------\n";
+	cout<<"  Retire Program\n";
+	cout<<"------------------\n";
 	cout<<"Age: ";
 	cin>>age;
 	wretire=(60-age)*52;
 	cout<<"Weeks to retire: "<<wretire;
+	cout<<"\n";
 	cout<<"\n";
 	return 0;
 }
